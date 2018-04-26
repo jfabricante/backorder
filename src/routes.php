67a09e336d2697e6aad98f8b0ef5001ff039a79b
Unknown <jerome-fabricante@isuzuphil.com>
2018-04-26 11:59:33 +0800

    Almighty push

--- src/routes.php
+++ src/routes.php
@@ -6,20 +6,28 @@ use Slim\Http\Response;
 // Routes
 
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+	// Sample log message
+	$this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+	// Render index view
+	return $this->renderer->render($response, 'index.phtml', $args);
 });
 
 
 $app->get('/test/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route '/' test");
+	// Sample log message
+	$this->logger->info("Slim-Skeleton '/' route '/' test");
 
-    $args['items'] = $this->db->table('t_pbo_order_item')->limit(10)->get();
+	$args['items'] = $this->db->table('t_pbo_order_item')->limit(10)->get();
 
-    echo __DIR__ '/..'; die;
-    return $this->renderer->render($response, 'index.phtml', $args);
+	echo __DIR__ ; die;
+	return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+	// Sample log message
+	$this->logger->info("Slim-Skeleton '/' route");
+
+	// Render index view
+	return $this->renderer->render($response, 'index.phtml', $args);
 });
