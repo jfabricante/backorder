6bcfdf5d885a5d05708a3ac4cab6bbf491796ead
Unknown <jerome-fabricante@isuzuphil.com>
2018-04-27 17:07:45 +0800

    Almighty push

--- src/routes.php
+++ src/routes.php
@@ -20,14 +20,41 @@ $app->get('/test/[{name}]', function (Request $request, Response $response, arra
 
 	$args['items'] = $this->db->table('t_pbo_order_item')->limit(10)->get();
 
-	echo __DIR__ ; die;
+	echo $request->getUri()->getBasePath(); die;
 	return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/dashboard/', function (Request $request, Response $response, array $args) {
 	// Sample log message
 	$this->logger->info("Slim-Skeleton '/' route");
 
 	// Render index view
-	return $this->renderer->render($response, 'index.phtml', $args);
+	return $this->renderer->render($response, 'backorder/index.phtml', $args);
+});
+
+$app->post('/store_resource/', function(Request $request, Response $response, array $args) {
+	ini_set('memory_limit', '-1');
+	ini_set('max_execution_time', 0);
+
+	$data = json_decode(file_get_contents("php://input"), true);
+
+	$total_count = count($data);
+
+	$limit = 0;
+
+	$table = $this->db->table('t_pbo_order_item');
+
+	$updated = array();
+
+	foreach($data as $item)
+	{
+		if (isset($item['Po No']) && isset($item['Part Number']) && isset($item['SO No']) && isset($item['New ETA']))
+		{
+			$table->where('order_no', $item['Po No'])->where('part_no', $item['Part Number'])->update(array('eta' => $item['New ETA']));
+			$limit++;
+		}
+	}
+	echo '<pre>';
+	echo 'success!';
+	echo '</pre>';
 });
